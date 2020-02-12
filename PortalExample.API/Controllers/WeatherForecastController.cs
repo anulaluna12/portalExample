3e44b5d2ee53dfbe83610495a80a4a826e87e52c
Katarzyna Bulik <katarzyna.bulik@accenture.com>
2020-02-12 14:54:47 +0100

    Zmiany metody w controllerz

--- PortalExample.API/Controllers/WeatherForecastController.cs
+++ PortalExample.API/Controllers/WeatherForecastController.cs
@@ -4,37 +4,58 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using PortalExample.API.Data;
+using PortalExample.API.Models;
 
 namespace PortalExample.API.Controllers
 {
     //http://localhost:5000/api/WeatherForecast po takim adresem nasłuchuje kestrel
     [ApiController]
     [Route("api/[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ValueController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<ValueController> _logger;
+        private readonly DataContext _context;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public ValueController(ILogger<ValueController> logger, DataContext context)
         {
+            _context = context;
+
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult GetValues()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult AddValue([FromBody] Value value)
+        {
+            _context.Values.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
+        }
+        [HttpPut("{id}")]
+        public IActionResult EditValue(int id, [FromBody] Value value)
+        {
+            var data = _context.Values.FirstOrDefault(x => x.Id == id);
+            data.Name = value.Name;
+
+            _context.SaveChanges();
+            return Ok(value);
         }
     }
 }
