1a45e02e38c338d5060ae3ea5671e3ba44e40a37
Katarzyna Bulik <katarzyna.bulik@accenture.com>
2020-02-13 09:53:12 +0100

    Dodanie do bazy user

--- PortalExample.API/Data/DataContext.cs
+++ PortalExample.API/Data/DataContext.cs
@@ -2,9 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using PortalExample.API.Models;
 namespace PortalExample.API.Data
 {
-    public class DataContext: DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options):base(options){           }
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
