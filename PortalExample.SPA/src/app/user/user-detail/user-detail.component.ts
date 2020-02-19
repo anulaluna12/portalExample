948a0406c263a40adea59aecb2054cb329acc28c
anulaluna12 <katarzyna.bulik@accenture.com>
2020-02-19 14:16:40 +0100

    poprawka do user detail komponet

--- PortalExample.SPA/src/app/user/user-detail/user-detail.component.ts
+++ PortalExample.SPA/src/app/user/user-detail/user-detail.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { User } from "src/app/_models/User";
-import { UserService } from "src/app/_services/user.service";
-import { AlerifyService } from "src/app/_services/alerify.service";
-import { ActivatedRoute } from "@angular/router";
-import { error } from "protractor";
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/_models/User';
+import { UserService } from 'src/app/_services/user.service';
+import { AlerifyService } from 'src/app/_services/alerify.service';
+import { ActivatedRoute } from '@angular/router';
+import { error } from 'protractor';
 
 @Component({
-  selector: "app-user-detail",
-  templateUrl: "./user-detail.component.html",
-  styleUrls: ["./user-detail.component.css"]
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
   user: User;
@@ -23,7 +23,7 @@ export class UserDetailComponent implements OnInit {
 
   }
   loadUser() {
-    this.userSer.getUser(this.route.snapshot.params['id']).subscribe(
+    this.userSer.getUser(this.route.snapshot.params.id).subscribe(
       (user: User) => {
         this.user = user;
       }, error => {
