60b1c648e5fa2fdea0a9d8ddd19a145833482f36
anulaluna12 <katarzyna.bulik@accenture.com>
2020-02-24 15:20:03 +0100

    ReactiveForm

--- PortalExample.SPA/src/app/app.module.ts
+++ PortalExample.SPA/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { JwtModule } from '@auth0/angular-jwt';
 import { RouterModule } from '@angular/router';
@@ -52,6 +52,7 @@ export function tokenGetter3() {
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     FileUploadModule,
     JwtModule.forRoot({
       config: {
--- PortalExample.SPA/src/app/register/register.component.ts
+++ PortalExample.SPA/src/app/register/register.component.ts
@@ -1,25 +1,37 @@
-import { AlerifyService } from './../_services/alerify.service';
-import { AuthService } from './../_services/auth.service';
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { AlerifyService } from "./../_services/alerify.service";
+import { AuthService } from "./../_services/auth.service";
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
   model: any = {};
   @Output() cancelRegister = new EventEmitter();
-  constructor(private authService: AuthService, private alertify: AlerifyService) { }
+  registerForm: FormGroup;
+  constructor(
+    private authService: AuthService,
+    private alertify: AlerifyService
+  ) {}
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
   }
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertify.success('Rejestracja udana');
-    }, error => {
-     this. alertify.error(error);
-    }
-    );
+    // this.authService.register(this.model).subscribe(
+    //   () => {
+    //     this.alertify.success("Rejestracja udana");
+    //   },
+    //   error => {
+    //     this.alertify.error(error);
+    //   }
+    // );
   }
   cancel() {
     this.cancelRegister.emit(false);
